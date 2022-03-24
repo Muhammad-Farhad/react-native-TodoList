a1aa737089e74374df0a0a6db4d3cac99ec57518
muhammad farhad <farhad@aisoftwares.co.in>
2022-03-24 09:23:50 +0500

    new commit

---
+++ tempData.js
@@ -0,0 +1,91 @@
+export default tempData = [
+    {
+        name: "Plan a Trip",
+        color: "#24A6D9",
+        todos:[
+            {
+                title: "Book Flight",
+                completed: false
+            },
+            {
+                title: "Passport Check",
+                completed: true
+            },
+            {
+                title: "Reserve Hotel Room",
+                completed: true
+            },
+            {
+                title: "Pack Luggage",
+                completed: false
+            }
+        ]
+    },
+    {
+        name: "Errands",
+        color: "#24A6D9",
+        todos:[
+            {
+                title: "Buy Milk",
+                completed: false
+            },
+            {
+                title: "Passport Check",
+                completed: true
+            },
+            {
+                title: "Reserve Hotel Room",
+                completed: true
+            },
+            {
+                title: "Pack Luggage",
+                completed: false
+            }
+        ]
+    },
+    {
+        name: "Errands",
+        color: "#24A6D9",
+        todos:[
+            {
+                title: "Buy Milk",
+                completed: false
+            },
+            {
+                title: "Passport Check",
+                completed: true
+            },
+            {
+                title: "Reserve Hotel Room",
+                completed: true
+            },
+            {
+                title: "Pack Luggage",
+                completed: false
+            }
+        ]
+    },
+    {
+        name: "Errands",
+        color: "#24A6D9",
+        todos:[
+            {
+                title: "Buy Milk",
+                completed: false
+            },
+            {
+                title: "Passport Check",
+                completed: true
+            },
+            {
+                title: "Reserve Hotel Room",
+                completed: true
+            },
+            {
+                title: "Pack Luggage",
+                completed: false
+            }
+        ]
+    },
+]
+    
